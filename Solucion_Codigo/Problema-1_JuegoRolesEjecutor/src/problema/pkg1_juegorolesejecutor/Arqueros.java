150d1b7e1b4a2c065b27ade1627a510766127531
juampyz7 <jlojaquichimbo@gmail.com>
2026-06-18 08:28:30 -0500

    Inicio Proyecto

---
+++ Solucion_Codigo/Problema-1_JuegoRolesEjecutor/src/problema/pkg1_juegorolesejecutor/Arqueros.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problema.pkg1_juegorolesejecutor;
+
+/**
+ *
+ * @author Usuario iTC
+ */
+public class Arqueros {
+    
+}
